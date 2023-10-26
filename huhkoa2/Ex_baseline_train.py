92504ac555cc806ab5cf8a89b34f512b14c52811
Ariana Huhko <huhkoa2@sunypoly.edu>
2023-10-26 17:13:32 -0400

    example baseline

---
+++ huhkoa2/Ex_baseline_train.py
@@ -0,0 +1,8 @@
+import CG.train as CGT
+
+def main(args):
+    CGT.main(["--dataroot", "./data"])
+    
+#code that will run if you run it directly; will not run if you import it
+if __name__ == "__main__":
+    main()
