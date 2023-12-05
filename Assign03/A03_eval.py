775b0a25ad801fd0870842c0746fa18256b78224
Ariana Huhko <huhkoa2@sunypoly.edu>
2023-12-05 17:36:44 -0500

    Display FID and KID scores

--- Assign03/A03_eval.py
+++ Assign03/A03_eval.py
@@ -1,9 +1,19 @@
 import torch_fidelity as torf
+from torch.utils.data import Dataset
+import os
+from torchvision import transforms
+import torch
+import cv2
 
-metrics = torf.calculate_metrics(
-    input1="gen_images/",
-    input2="train_images/",
-    fid=True,
-    kid=True,
-    kid_subset_size=700,
-    cuda=True)
+def main():
+    metrics = torf.calculate_metrics(
+        input1="Assign03/gen_images",
+        input2="Assign03/train_images",
+        fid=True,
+        kid=True,
+        kid_subset_size=200,
+        cuda=True)
+
+
+if __name__ == "__main__":
+    main()
